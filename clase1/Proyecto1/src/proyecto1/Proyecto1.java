97c6fe2fd9fce0b05bd0c6caa983620b5ad59aaf
Milton Paredes <mjparedes11@utpl.edu.ec>
2017-10-05 11:31:42 -0500

    Programa de tabla de multiplicación

--- clase1/Proyecto1/src/proyecto1/Proyecto1.java
+++ clase1/Proyecto1/src/proyecto1/Proyecto1.java
@@ -5,6 +5,8 @@
  */
 package proyecto1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author MIlton Paredes
@@ -15,7 +17,15 @@ public class Proyecto1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada= new Scanner(System.in);
+        int num;
+        int val;
+        System.out.println("Ingrese un numero:");
+        num= entrada.nextInt();
+        for (int i = 0; i < 13; i++) {
+            val= num *i;
+            System.out.println(num+"*"+i+"="+val);
+        }
     }
     
 }
